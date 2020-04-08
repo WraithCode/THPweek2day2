0b919455888502c8efa5a76253a2cc0481798a87
WraithCode <bilaldu34@outlook.fr>
2020-04-08 23:43:36 +0200

    add 01_pyramids

--- w2d2/lib/01_pyramids.rb
+++ w2d2/lib/01_pyramids.rb
@@ -27,27 +27,34 @@ def half_pyramid
 end
 
 def full_pyramid(height)
-    puts "Salut, bienvenue dans la pyramide, combien d'étages veux-tu ? Entre un nombre impair"
-    print "> "
-    etages = gets.to_i
-    if ((etages%2) != 0) 
-        puts "Envoie un nombre impair je t'ai dit !!!! (stp sinon t'auras jamais de pyramide)"
-    end
-        puts "Voici la pyramide :"
-        i = etages
-        a = 1 
-    while i>= 1
-        i.times do
-            print " "
-        end
-        2*a.times do 
-            print "#"
-        end
-        
-        puts
-        i = i/2
-        a = a+1
+    # dessine le haut de la pyramide
+    base_length = height * 2 - 1
+    i = 1 # Iteration for hashtag sign
+    
+    puts "Here's your pyramid master!"
+  
+    height.times do
+      puts " " * (Integer(base_length / 2) - Integer(i / 2)) + "#" * i
+      i += 2
     end
 end
 
-full_pyramid
+def type_pyramid
+    puts "Quelle pyramide désirez-vous ?"
+    puts "1 - Full"
+    puts "2 - Wtf"
+    choice = gets.chomp
+    if choice == "1"
+        height = pyramid_height
+        full_pyramid(height)
+    end
+    if choice == "2"
+        pyramid_height
+        puts "je ne l'ai pas fait hahaha"
+    else 
+        puts "veuillez entrer un choix entre 1 et 2"
+    end
+end
+
+
+type_pyramid
